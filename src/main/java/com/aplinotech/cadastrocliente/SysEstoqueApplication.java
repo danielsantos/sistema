c5eb4c63ef5438a9592e89cb9790ff65258b2740
Daniel Rocha <danielsantosr.rj@gmail.com>
2018-05-09 17:43:12 -0300

    desenv

--- src/main/java/com/aplinotech/cadastrocliente/SysEstoqueApplication.java
+++ src/main/java/com/aplinotech/cadastrocliente/SysEstoqueApplication.java
@@ -2,17 +2,10 @@ package com.aplinotech.cadastrocliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SysEstoqueApplication extends SpringBootServletInitializer {
+public class SysEstoqueApplication {
 	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SysEstoqueApplication.class);
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(SysEstoqueApplication.class, args);
 	}
